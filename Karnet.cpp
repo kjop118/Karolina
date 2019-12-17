bac8f5d22af0f26df728713725b94554625a5731
kjop118 <58214322+kjop118@users.noreply.github.com>
2019-12-17 12:12:33 +0100

    Create Karnet.cpp

---
+++ Karnet.cpp
@@ -0,0 +1,27 @@
+include <iostream>
+
+
+class Karnet
+{
+    public:
+    string data_rozp_karnetu;
+    string data_konca_karnetu;
+    int cena;
+    int ilosc;
+    
+    void kup_karnet()
+    {
+        cout>>"Wybierz rodzaj karnetu">>endl;
+        cout>>"1. Karnet czasowy">>endl;
+        cout>>"2. Karnet zjazdowy">>endl;
+    
+    }
+    void pokaz_dane():
+ };
+ 
+ int main()
+ {
+    
+  
+    return 0;
+ }
