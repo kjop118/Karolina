742bbcf9795d7bd38d4fdc203e6b1242ad8396e3
mkocurek7 <58214646+mkocurek7@users.noreply.github.com>
2020-01-14 12:38:52 +0100

    Update karnetCzasowy.cpp

--- karnetCzasowy.cpp
+++ karnetCzasowy.cpp
@@ -1,3 +1,16 @@
 
 #include <iostream>
 using namespace std;
+
+class Czasowy: public Dzienny, public Popoludniowy, public Nocny, public Calodniowy, public Godzinny
+{
+    public:
+  int dane_od;
+  int dane_do;
+};
+
+class Dzienny{};
+class Popoludniowy{};
+class Nocny{};
+class Calodniowy{};
+class Godzinny{};
